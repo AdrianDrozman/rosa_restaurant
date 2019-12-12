a3c71fccf81813e5341a2845a93232c2f8baf586
AdrianDrozman <adytestare@gmail.com>
2019-12-12 22:34:26 +0200

    finish hero section + nav

---
+++ main.js
@@ -0,0 +1,8 @@
+//Select element function
+
+let menuToggler = document.querySelector('.menu-toggle');
+let body = document.querySelector('body');
+
+menuToggler.addEventListener('click', function() {
+  body.classList.toggle('open');
+});

0959e288eaec3b645f567587f2c5c8308ac4abd8
AdrianDrozman <adytestare@gmail.com>
2019-12-12 22:53:42 +0200

    fixed the side menu bug

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 //Select element function
+const selectElement = function(element) {
+  return document.querySelector(element);
+};
 
-let menuToggler = document.querySelector('.menu-toggle');
-let body = document.querySelector('body');
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
 
 menuToggler.addEventListener('click', function() {
   body.classList.toggle('open');
